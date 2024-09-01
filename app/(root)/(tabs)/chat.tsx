295475a79740a18e2d9e6cb884223a8ea1467257
Emmanuel-xt <fattmane5@gmail.com>
2024-09-02 00:18:47 +0100

    implement welcome screen

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Redirect } from "expo-router";
 
-const Home = () => {
-  return <Redirect href="/(auth)/welcome" />;
+const Chaat = () => {
+  return (
+    <SafeAreaView>
+      <Text>Chaat</Text>
+    </SafeAreaView>
+  );
 };
 
-export default Home;
+export default Chaat;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,6 +35,8 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" />
+      <Stack.Screen name="(auth)" />
       <Stack.Screen name="+not-found" />
     </Stack>
   );

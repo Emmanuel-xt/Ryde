ef988cdec0eebc88dae1f5dc15b4fd8e0888aeea
Emmanuel-xt <fattmane5@gmail.com>
2024-09-03 01:22:55 +0100

    setup neon db and create user

---
+++ app/(api)/user+api.ts
@@ -0,0 +1,28 @@
+import { neon } from '@neondatabase/serverless';
+
+export async function POST(request: Request) {
+    try {
+        const sql = neon(`${process.env.DATABASE_URL}`);
+
+        const { name, email, clerkId } = await request.json();
+
+        if (!name || !email || !clerkId) {
+            return Response.json({ error: "Missing required fields" });
+        }
+
+        const response = await sql`
+        INSERT INTO users (
+            name, email, clerk_id
+        )
+        VALUES (
+            ${name},
+            ${email},
+            ${clerkId}
+        )`;
+
+        return new Response(JSON.stringify({ data: response }), { status: 201 });
+    } catch (error: any) {
+        console.log("error inserting user", error);
+        return Response.json({ error: error.message }, { status: 500 });
+    }
+}
